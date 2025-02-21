73aee287a171ca3304aba34d901621b6dc8a4356
ankitgujre <kumarankit65494@gmail.com>
2025-02-21 22:56:59 +0530

    basic example of function

---
+++ FuncionBasic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void greet(){
+    printf("Hello World");
+    printf("How are you?");
+    return;  // return statement is optional since it is the last statement in the function declaration 
+}
+int main(){
+    greet();  // function call is optional since it is the first statement in the main function declaration
+    printf("\n");
+    greet();
+    return 0;
+}
